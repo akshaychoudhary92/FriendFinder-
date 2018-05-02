121a15876f42c7d283a1e173398d9cb04680af5d
Akshay Choudhary <ak92choudhary@gmail.com>
2018-05-02 19:08:01 -0400

    complete

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+    {
+      name: "dave",
+      photo: "",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    }
+
+  ];
+
+
+  module.exports = friends;
